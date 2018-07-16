67778f653eaf6a77ac0307e3560e172d968cbe03
Binary god <samoski93@hotmail.com>
2018-07-16 17:35:46 +0100

    Added entity classes needed to store data in the data sources

---
+++ VideoOnDemand.Data/Data/Entities/Download.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace VideoOnDemand.Data.Data.Entities
+{
+    public class Download
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(80), Required]
+        public string Description { get; set; }
+
+        [MaxLength(1024)]
+        public string Url { get; set; }
+
+        public Module Module { get; set; }
+        public int ModuleId { get; set; }
+
+        // Side-step from 3rd normal form for easier
+        // access to a video’s course
+        public Course Course { get; set; }
+        public int CourseId { get; set; }
+    }
+}
