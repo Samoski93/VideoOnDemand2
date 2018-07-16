69979e84ecddf2e90da4541b4e715ee643a20c82
Binary god <samoski93@hotmail.com>
2018-07-16 16:15:31 +0100

    Added ViewModels for the Membership view

---
+++ VideoOnDemand.UI/Models/MembershipViewModels/CourseModelView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VideoOnDemand.UI.Models.DTOModels;
+
+namespace VideoOnDemand.UI.Models.MembershipViewModels
+{
+    public class CourseModelView
+    {
+        public CourseDTO Course { get; set; }
+        public InstructorDTO Instructor { get; set; }
+        public IEnumerable<ModuleDTO> ModuleDTOs { get; set; }
+    }
+}
---
+++ VideoOnDemand.UI/Models/MembershipViewModels/DashboardViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VideoOnDemand.UI.Models.DTOModels;
+
+namespace VideoOnDemand.UI.Models.MembershipViewModels
+{
+    public class DashboardViewModel
+    {
+        // a list in a list - display three course panels on each row.
+        public List<List<CourseDTO>> Courses { get; set; }
+    }
+}
